f40bc59c0ae3f66dd2943140f26d1f070d40db17
nedaniku <neda.nikooy78@gmail.com>
2024-04-30 22:11:07 +0330

    smart

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'blue-dark' : '#1E3756',
+      'yellow' : '#E7FE58',
+      'white' : '#F0F3FF',
+      'light-blue' : '#6A7CBC',
+      'pure-w' : '#ffff'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
